150039b9cb3be40d415b09994e532a5b27cce8c8
dummiebrummie <91922337+dummiebrummie@users.noreply.github.com>
2024-03-11 13:39:22 -0400

    Delete Professor.py

--- Professor.py
+++
@@ -1,5 +0,0 @@
-class Professor(User):
-    def __init__(self, user_id, username, email, password, department, office_hours):
-        super().__init__(user_id, username, email, password)
-        #self.department = department
-        #self.office_hours = office_hours
