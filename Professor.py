d026e88f973e822fd348d614810bb6bf055293fd
dummiebrummie <91922337+dummiebrummie@users.noreply.github.com>
2024-03-11 13:47:06 -0400

    password remove

--- Professor.py
+++ Professor.py
@@ -1,5 +1,5 @@
 class Professor(User):
-    def __init__(self, user_id, username, email, password, department, office_hours):
-        super().__init__(user_id, username, email, password)
+    def __init__(self, user_id, username, email, department, office_hours):
+        super().__init__(user_id, username, email)
         #self.department = department
         #self.office_hours = office_hours
